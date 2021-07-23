6ae0b91034d425f3b2ffd52ea6965b60d855ee2f
takokun778 <takokun.778@gmail.com>
2021-07-23 17:35:23 +0900

    feat partial-constructor

---
+++ src/partial-constructor.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon';
+import * as uuid from 'uuid';
+
+export class User {
+    id!: string;
+    lastName!: string;
+    firstName!: string;
+    birthday!: DateTime;
+
+    private constructor(init: Partial<User>) {
+        Object.assign(this, init);
+    }
+
+    static new(init: Partial<User>) {
+        return new User({
+            id: uuid.v4(),
+            lastName: init.lastName,
+            firstName: init.firstName,
+            birthday: init.birthday,
+        });
+    }
+
+    update(props: Partial<Omit<User, 'id'>>) {
+        return new User({
+            id: this.id,
+            lastName: props.lastName ?? this.lastName,
+            firstName: props.firstName ?? this.firstName,
+            birthday: props.birthday ?? this.birthday,
+        })
+    }
+}
